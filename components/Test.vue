008d80fbdbce1ade1eafac939625aa6424e47121
jongjanja <narinohm123@gmail.com>
2021-07-28 14:45:32 +0700

    Nuxt

---
+++ components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-app-bar color="#F06292 " >Hello</v-app-bar>
+
+  </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/cos/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="rounded-lg shadow-md p-4 m-6 bg-white border-b-4 border-green-500">
+      <h2>Name</h2>
+      <h2>ID</h2>
+      <h3>Teacher</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        name : { default : "Unknow name" },
+        id : { default : "Unknow id" },
+        teacher : { default : "Unknow teacher" },
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
